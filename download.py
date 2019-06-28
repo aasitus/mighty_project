be1834d900dd676ab67b96151cf0d46510c87323
Kekkonen Arto I <arkekkon@lx8-700-001.valt.helsinki.fi>
2019-06-28 14:23:00 +0200

    Nicer code

--- download.py
+++ download.py
@@ -1,66 +1,96 @@
-import psaw
+# Functions for downloading Reddit stuff from Pushshift
+
 import pandas as pd
+import psaw
+import datetime
 
-def download_comments(subreddit, chunk_size, after):
+api = psaw.PushshiftAPI()
 
-    api = psaw.PushshiftAPI()
+def get_comments(subreddit, n_comments, after):
+
+    # Gets a certain number of comments from a subreddit
+    # and returns them in a dataframe
+
+    df = pd.DataFrame(
+        api.search_comments(
+        subreddit = subreddit,
+        sort = 'asc',
+        sort_type = 'created_utc',
+        filter=['author','body','subreddit',
+                'score', 'level', 'submit_text',
+                'parent_id', 'link_id','id'],
+        after = after,
+        limit = n_comments))
+
+    return df
+
+def get_submissions(subreddit, n_submissions, after):
+
+    # Gets a certain number of submissions froma subreddit
+    # and returns them in a dataframe
+
+    df = pd.DataFrame(
+        api.search_submissions(
+            subreddit = subreddit,
+            sort = 'asc',
+            sort_type = 'created_utc',
+            filter = ['id', 'url','author',
+                'title', 'subreddit', 'score',
+                'num_comments'],
+            after = after,
+            limit = n_submissions))
+    return df
+
+def subreddit_comments(subreddit, chunk_size, after, data_path = '', verbose = True):
+
+    # Downloads all comments from a subreddit
+    # beginning from after
 
     after = after
 
     while True:
 
-        df = pd.DataFrame(
-            api.search_comments(
-            subreddit = subreddit,
-            sort = 'asc',
-            sort_type = 'created_utc',
-            filter=['author','body','subreddit',
-                    'score', 'level', 'submit_text',
-                    'parent_id', 'link_id','id'],
-            after = after,
-            limit = chunk_size))
+        print(chunk_size)
+
+        df = get_comments(subreddit, chunk_size, after)
 
         start = df.iloc[0]['created_utc']
         end = df.iloc[-1]['created_utc']
 
-        filename = 'comments/' + subreddit + '_comments_' + str(start) + '_' + str(end) + '.csv'
+        filename = data_path + subreddit + '_comments_' + str(start) + '_' + str(end) + '.csv'
         df.to_csv(filename)
 
         after = end
 
-        print(after)
+        if verbose:
+            print('Now at ' + str(datetime.datetime.utcfromtimestamp(after)))
 
         if len(df) < chunk_size:
             break
 
-def download_submissions(subreddit, chunk_size, after):
+def subreddit_submissions(subreddit, chunk_size, after, data_path = '', verbose = True):
 
-    api = psaw.PushshiftAPI()
+    # Downloads all submissions from a subreddit
+    # beginning from after
 
     after = after
 
     while True:
 
-        df = pd.DataFrame(
-            api.search_submissions(
-                subreddit = subreddit,
-                sort = 'asc',
-                sort_type = 'created_utc',
-                filter = ['id', 'url','author',
-                    'title', 'subreddit', 'score',
-                    'num_comments'],
-                after = after,
-                limit = chunk_size))
+        print(chunk_size)
+
+        df = get_submissions(subreddit, chunk_size, after)
 
         start = df.iloc[0]['created_utc']
         end = df.iloc[-1]['created_utc']
 
-        filename = 'posts/' + subreddit + '_posts_' + str(start) + '_' + str(end) + '.csv'
+        filename = data_path + subreddit + '_submissions_' + str(start) + '_' + str(end) + '.csv'
         df.to_csv(filename)
 
         after = end
 
-        print(after)
+        if verbose:
+            print('Now at ' + str(datetime.datetime.utcfromtimestamp(after)))
 
         if len(df) < chunk_size:
             break
